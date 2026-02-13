ffef28bc1e81d09d9d86af3695d671ce4ef2ec9a
didac <diru046@vidalibarraquer.net>
2026-02-13 08:26:52 +0100

    Afegit

--- src/main/java/org/yourcompany/yourproject/Pregunta.java
+++ src/main/java/org/yourcompany/yourproject/Pregunta.java
@@ -2,5 +2,40 @@ package org.yourcompany.yourproject;
 
 public class Pregunta {
     private String enunciat;
+    private String[] respostes;
+    private int correcta;
+
+    public Pregunta(int correcta, String enunciat, String[] respostes) {
+        this.correcta = correcta;
+        this.enunciat = enunciat;
+        this.respostes = respostes;
+    }
+
+    public String getEnunciat() {
+        return enunciat;
+    }
+
+    public void setEnunciat(String enunciat) {
+        this.enunciat = enunciat;
+    }
+
+    public String[] getRespostes() {
+        return respostes;
+    }
+
+    public void setRespostes(String[] respostes) {
+        this.respostes = respostes;
+    }
+
+    public int getCorrecta() {
+        return correcta;
+    }
+
+    public void setCorrecta(int correcta) {
+        this.correcta = correcta;
+    }
+    
+
+
     
 }
---
+++ src/main/java/org/yourcompany/yourproject/Test.java
@@ -0,0 +1,11 @@
+package org.yourcompany.yourproject;
+
+import java.util.ArrayList;
+
+public class Test {
+
+    private ArrayList<Pregunta> preguntes;
+    private int[] respostesUsuari;
+    private int numPregunta;
+    
+}
